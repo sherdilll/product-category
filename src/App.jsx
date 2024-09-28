5eb3b5bd4bad340356d743f5d40d74fe551cae9b
Divesh Kumar <104316863+sherdilll@users.noreply.github.com>
2024-09-28 19:59:35 +0530

    Add comments to App.jsx that explains limitations of the app

--- src/App.jsx
+++ src/App.jsx
@@ -14,3 +14,7 @@ const App = () => {
 };
 
 export default App;
+// 2. The app currently does not handle error states well (e.g., when the API request fails).
+// 3. Infinite scrolling is not yet implemented.
+// 4. There's no clear feedback if no products are available (a message could be displayed).
+// 5. Since we're using client-side pagination, it could become inefficient with very large data sets.
